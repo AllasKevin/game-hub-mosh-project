f88d096181152a82b884390267ec30e8cef96459
AllasKevin <kevin_ruonala@hotmail.com>
2025-03-11 16:49:52 +0100

    Created a custom hook to fetch the games.

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,51 @@
+import apiClient from "@/services/api-client";
+import { useEffect, useState } from "react";
+import CanceledError from "axios";
+import axios from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  const fetchGames = (controller: AbortController) => {
+    apiClient
+    .get<FetchGamesResponse>("/games", {signal: controller.signal})
+    .then((res) => {
+      const modifiedGames = res.data.results.map((game) => ({
+        ...game,
+        id: game.id * Math.random(), // Example transformation
+      }));
+
+      setGames((prevGames) => [...prevGames, ...modifiedGames]);
+    })
+    .catch((err) => {
+      if(axios.isCancel(err)) return;
+      console.log("setting error to: " + err.message);
+      setError(err.message)});
+
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetchGames(controller);
+    fetchGames(controller);
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error};
+}
+
+export default useGames;
